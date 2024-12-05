f96197eecbc164aec3454130d04fe7b2d72e6727
César Guerra <warderer@gmail.com>
2024-12-04 19:59:11 -0600

    ✨ Add AuthContext implementation

--- 03-react-auth/src/context/AuthContext.jsx
+++ 03-react-auth/src/context/AuthContext.jsx
@@ -1,4 +1,5 @@
-import { createContext } from 'react'
+import { createContext, useState, useEffect } from 'react'
+import { jwtDecode } from 'jwt-decode'
 
 // 1. Crear el contexto
 const AuthContext = createContext()
@@ -6,9 +7,38 @@ const AuthContext = createContext()
 // 2. Crear el Proveedor (provider) del contexto.
 function AuthProvider ({children}) {
 // Aquí va mi lógica de autenticación
+    const [isAuth, setIsAuth] = useState(false) // ¿Estoy autenticado?
+    const [userPayload, setUserPayload] = useState(null) // JWT Payload decodicado
+
+    const login = (token) => {
+        localStorage.setItem('token', token)
+        const payload = jwtDecode(token)
+        setIsAuth(true)
+        setUserPayload(payload)
+    }
+
+    const logout = () => {
+        localStorage.removeItem('token')
+        setIsAuth(false)
+        setUserPayload(null)
+    }
+
+    // Verificar si existe un token en el localStorage, y si es válido lo va a cargar para evitar que el usuario tenga que volver a iniciar sesión.
+    useEffect(() => {
+        const token = localStorage.getItem('token')
+        if (token) {
+            const payload = jwtDecode(token)
+            setIsAuth(true)
+            setUserPayload(payload)
+        }
+    }, [])
 
     const data = {
         // Aquí van los datos que quiero compartir de forma global
+        isAuth,
+        userPayload,
+        login,
+        logout
     }
 
     return (
