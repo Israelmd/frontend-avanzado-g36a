3807834d5917841bb8deb5e2baa5179119c5a5d1
César Guerra <warderer@gmail.com>
2024-12-09 21:09:17 -0600

    ✅ Add Login as Customer Test

--- 03-react-auth/cypress/e2e/auth.cy.js
+++ 03-react-auth/cypress/e2e/auth.cy.js
@@ -8,4 +8,24 @@ describe('Funcionalidad de Login', () => {
     cy.get('h1')
       .contains('Home') // 03. Assert: Verificar que la app se comporta como esperamos
   })
+
+  it('Probar el Login como CUSTOMER', () => {
+    cy.intercept('POST','https://ecommerce-json-jwt.onrender.com/login').as('login')
+    // 01. Arrange: Setup del estado inicial de la app
+    cy.visit('/login')
+
+    // 02. Act: Interactuar con la app (realizar acciones)
+    cy.get('input[type="email"]')
+      .type('drstrange@marvel.com')
+    cy.get('input[type="password"]')
+      .type('multiverso')
+
+    cy.get('button[type="submit"]')
+      .click()
+    cy.wait('@login')
+
+    // 03. Assert: Verificar que la app se comporta como esperamos
+    cy.get('h1')
+      .contains('Dashboard')
+  })
 })
