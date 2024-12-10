e2b090d1694c9cce9907e446ad4f4b20f45c2fd6
César Guerra <warderer@gmail.com>
2024-12-09 21:19:56 -0600

    ✅ Add Logout as Admin test

--- 03-react-auth/cypress/e2e/auth.cy.js
+++ 03-react-auth/cypress/e2e/auth.cy.js
@@ -28,4 +28,27 @@ describe('Funcionalidad de Login', () => {
     cy.get('h1')
       .contains('Dashboard')
   })
+
+  it('Probar el Logout como Admin y me debe llevar a Login', () => {
+    cy.intercept('POST','https://ecommerce-json-jwt.onrender.com/login').as('login')
+    // 01. Arrange: Setup del estado inicial de la app
+    cy.visit('/login')
+
+    // 02. Act: Interactuar con la app (realizar acciones)
+    cy.get('input[type="email"]')
+      .type('superman@dc.com')
+    cy.get('input[type="password"]')
+      .type('superman')
+
+    cy.get('button[type="submit"]')
+      .click()
+    cy.wait('@login')
+
+    cy.get('nav > ul li:last')
+      .click()
+
+    // 03. Assert: Verificar que la app se comporta como esperamos
+    // cy.get('h1').contains('Please sign in')
+    cy.url().should('include', '/login')
+  })
 })
